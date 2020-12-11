c5a9588eaa266b17b48b5cd93393fad7bca4afc2
Alvaro Argañaraz <arganarazalvaro@gmail.com>
2020-12-11 09:51:11 -0300

    Add passport and express-sesion to package.json

--- server.js
+++ server.js
@@ -5,8 +5,20 @@ const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
+const session = require('express-session');
+const passport = require('passport');
 
 app.set('view engine', 'pug')
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
